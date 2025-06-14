dd75bce62b98416772ab207d12084ad9532c192e
Leynos <leynos@troubledskies.net>
2025-06-14 12:44:11 +0100

    Refactor preamble reader

--- src/preamble.rs
+++ src/preamble.rs
@@ -4,6 +4,43 @@ use tokio::io::{self, AsyncRead, AsyncReadExt};
 
 const MAX_PREAMBLE_LEN: usize = 1024;
 
+async fn read_more<R>(
+    reader: &mut R,
+    buf: &mut Vec<u8>,
+    additional: usize,
+) -> Result<(), DecodeError>
+where
+    R: AsyncRead + Unpin,
+{
+    let start = buf.len();
+    if start + additional > MAX_PREAMBLE_LEN {
+        return Err(DecodeError::Other("preamble too long"));
+    }
+    buf.resize(start + additional, 0);
+    let mut read = 0;
+    while read < additional {
+        match reader
+            .read(&mut buf[start + read..start + additional])
+            .await
+        {
+            Ok(0) => {
+                return Err(DecodeError::Io {
+                    inner: io::Error::from(io::ErrorKind::UnexpectedEof),
+                    additional: additional - read,
+                });
+            }
+            Ok(n) => read += n,
+            Err(inner) => {
+                return Err(DecodeError::Io {
+                    inner,
+                    additional: additional - read,
+                });
+            }
+        }
+    }
+    Ok(())
+}
+
 /// Read and decode a connection preamble using bincode.
 ///
 /// This helper reads the exact number of bytes required by `T`, as
@@ -31,32 +68,7 @@ where
                 return Ok((value, leftover));
             }
             Err(DecodeError::UnexpectedEnd { additional }) => {
-                let start = buf.len();
-                if start + additional > MAX_PREAMBLE_LEN {
-                    return Err(DecodeError::Other("preamble too long"));
-                }
-                buf.resize(start + additional, 0);
-                let mut read = 0;
-                while read < additional {
-                    match reader
-                        .read(&mut buf[start + read..start + additional])
-                        .await
-                    {
-                        Ok(0) => {
-                            return Err(DecodeError::Io {
-                                inner: io::Error::from(io::ErrorKind::UnexpectedEof),
-                                additional: additional - read,
-                            });
-                        }
-                        Ok(n) => read += n,
-                        Err(inner) => {
-                            return Err(DecodeError::Io {
-                                inner,
-                                additional: additional - read,
-                            });
-                        }
-                    }
-                }
+                read_more(reader, &mut buf, additional).await?;
             }
             Err(e) => return Err(e),
         }
